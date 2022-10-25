ba88eab0d2484322d831d745660e397415e318f1
Yuriy Chebyshev <bomzheg@gmail.com>
2022-10-25 23:12:28 +0300

    fix pipeline =(

--- db/dao/rdb/game.py
+++ db/dao/rdb/game.py
@@ -7,7 +7,6 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import joinedload
 
 from db import models
-from shvatka.dal.game import ActiveGameFinder, GameByIdGetter, WaiverStarter
 from shvatka.models import dto
 from shvatka.models.dto.scn.game import GameScenario
 from shvatka.models.enums import GameStatus
@@ -15,7 +14,7 @@ from shvatka.models.enums.game_status import ACTIVE_STATUSES
 from .base import BaseDAO
 
 
-class GameDao(BaseDAO[models.Game], ActiveGameFinder, GameByIdGetter, WaiverStarter):
+class GameDao(BaseDAO[models.Game]):
     def __init__(self, session: AsyncSession):
         super().__init__(models.Game, session)
 

15d945e4c506985386486129976343ebb5346373
Yuriy Chebyshev <bomzheg@gmail.com>
2022-11-05 21:27:17 +0300

    fix test. Thanks to @jorektheglitch

--- tests/integration/test_game_stat.py
+++ tests/integration/test_game_stat.py
@@ -1,4 +1,5 @@
-from functools import reduce
+import operator
+from itertools import starmap, pairwise
 
 import pytest
 
@@ -12,4 +13,4 @@ async def test_game_level_times(completed_game: dto.FullGame, dao: HolderDao):
     game_stat = await get_game_stat(completed_game, dao.level_time)
     for team, level_times in game_stat.level_times.items():
         assert all(team.id == lt.team.id for lt in level_times)
-        assert reduce(lambda x, y: x.level_number < y.level_number, level_times)
+        assert all(starmap(operator.lt, pairwise(map(lambda lt: lt.level_number, level_times))))

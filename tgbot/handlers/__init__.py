45e1aab1bbf10033700da359af0db262d05d55cc
Yuriy Chebyshev <bomzheg@gmail.com>
2022-11-07 21:08:48 +0300

    few update structure

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -14,12 +14,11 @@ from tgbot.handlers import team
 logger = logging.getLogger(__name__)
 
 
-def setup_handlers(dp: Dispatcher, bot_config: BotConfig):
+def setup_handlers(dp: Dispatcher, bot_config: BotConfig, registry: DialogRegistry):
     errors.setup(dp, bot_config.log_chat)
     dp.include_router(base.setup())
     dp.include_router(superuser.setup(bot_config))
 
-    registry = DialogRegistry(dp)
     setup_dialogs(registry)
 
     dp.include_router(team.setup())
--- tgbot/main_factory.py
+++ tgbot/main_factory.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.base import BaseStorage
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
+from aiogram_dialog import DialogRegistry
 from dataclass_factory import Factory
 from redis.asyncio.client import Redis
 from sqlalchemy.orm import sessionmaker
@@ -40,6 +41,7 @@ def create_dispatcher(
     redis: Redis, scheduler: Scheduler, locker: KeyCheckerFactory, file_storage: FileStorage,
 ) -> Dispatcher:
     dp = Dispatcher(storage=create_storage(config.storage))
+    registry = DialogRegistry(dp)
     setup_middlewares(
         dp=dp,
         pool=pool,
@@ -51,7 +53,7 @@ def create_dispatcher(
         locker=locker,
         file_storage=file_storage,
     )
-    setup_handlers(dp, config.bot)
+    setup_handlers(dp, config.bot, registry)
     return dp
 
 

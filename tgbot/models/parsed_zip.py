79ffb4e92aa0285444931a8c723baddaf8c3d3da
Yuriy Chebyshev <bomzheg@gmail.com>
2022-11-10 00:13:14 +0300

    fix load zip as game

--- tgbot/models/parsed_zip.py
+++ tgbot/models/parsed_zip.py
@@ -17,8 +17,9 @@ class ParsedZip:
         contents = {}
         try:
             for guid, path in self.files.items():
-                contents[guid]: path.open()
-            yield RawGameScenario(scn=yaml.safe_load(self.scn), files=contents)
+                contents[guid] = path.open("rb")
+            with self.scn.open("r", encoding="utf8") as f:
+                yield RawGameScenario(scn=yaml.safe_load(f), files=contents)
         finally:
             for file in contents.values():
                 file.close()

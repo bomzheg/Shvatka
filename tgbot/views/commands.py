15f4b9c50b461a8318efdb11a34874ef8953233a
Yuriy Chebyshev <bomzheg@gmail.com>
2022-11-20 02:24:45 +0300

    add few TODO

--- tgbot/views/commands.py
+++ tgbot/views/commands.py
@@ -14,7 +14,7 @@ class CommandsGroup:
 
 
 START_COMMAND = BotCommand(command="start", description="начало работы с ботом")
-HELP_COMMAND = BotCommand(command="help", description="помощь")
+HELP_COMMAND = BotCommand(command="help", description="помощь")  # TODO
 ABOUT_COMMAND = BotCommand(command="about", description="о боте")
 CANCEL_COMMAND = BotCommand(command="cancel", description="отмена начатого диалога")
 CHAT_ID_COMMAND = BotCommand(command="chat_id", description="узнать chat_id данного чата")
@@ -28,25 +28,25 @@ ADD_IN_TEAM_COMMAND = BotCommand(
 )
 MANAGE_TEAM_COMMAND = BotCommand(
     command="manage_team", description="открыть меню управления командой"
-)
+)  # TODO
 REMOVE_FROM_TEAM_COMMAND = BotCommand(
     command="remove_from_team", description="удалить игрока из команды (реплаем по игроку)"
-)
+)  # TODO
 START_WAIVERS_COMMAND = BotCommand(command="waivers", description="начать сборку вейверов")
 APPROVE_WAIVERS_COMMAND = BotCommand(
     command="approve_waivers", description="закрыть сборку вейверов"
 )
 
-STATUS_COMMAND = BotCommand(command="status", description="статус схватки")
+STATUS_COMMAND = BotCommand(command="status", description="статус схватки")  # TODO
 TEAM_COMMAND = BotCommand(command="team", description="команда")
-TEAMS_COMMAND = BotCommand(command="teams", description="список команд")
+TEAMS_COMMAND = BotCommand(command="teams", description="список команд")  # TODO
 PLAYERS_COMMAND = BotCommand(command="players", description="игроки команды")
-ALL_PLAYERS_COMMAND = BotCommand(command="all_players", description="игроки команды")
-ME_COMMAND = BotCommand(command="me", description="мой профиль")
-LEAVE_COMMAND = BotCommand(command="leave", description="выйти из команды")
-GAMES_COMMAND = BotCommand(command="games", description="список игр")
+ALL_PLAYERS_COMMAND = BotCommand(command="all_players", description="игроки команды")  # TODO
+ME_COMMAND = BotCommand(command="me", description="мой профиль")  # TODO
+LEAVE_COMMAND = BotCommand(command="leave", description="выйти из команды")  # TODO
+GAMES_COMMAND = BotCommand(command="games", description="список игр")  # TODO
 
-GET_WAIVERS_COMMAND = BotCommand(command="get_waivers", description="показать текущие вейверы")
+GET_WAIVERS_COMMAND = BotCommand(command="get_waivers", description="показать текущие вейверы")  # TODO
 SPY_COMMAND = BotCommand(
     command="spy", description="Меню шпиона - организатора"
 )
@@ -61,23 +61,26 @@ HELP_BASE = CommandsGroup("Базовые команды:", [
     START_COMMAND, HELP_COMMAND, ABOUT_COMMAND, CANCEL_COMMAND, CHAT_ID_COMMAND,
     CHAT_TYPE_COMMAND,
 ])
-UPDATE_COMMANDS = BotCommand(command="update_commands", description="обновить команды бота")
+UPDATE_COMMANDS = BotCommand(command="update_commands", description="обновить команды бота")  # TODO
 EXCEPTION_COMMAND = BotCommand(command="exception", description="сгенерировать исключение")
 GET_OUT = BotCommand(command="get_out", description="бот, уходи из чата!")
+JOBS_COMMAND = BotCommand(command="jobs", description="запланированные функции")
+CANCEL_JOBS_COMMAND = BotCommand(command="cancel_jobs", description="отменить запланированные функции")
 HELP_ADMIN = CommandsGroup("Команды администратора бота:", [
-    BotCommand(command="jobs", description="запланированные функции"),
-    BotCommand(command="cancel_jobs", description="отменить запланированные функции"),
+    JOBS_COMMAND,
+    CANCEL_JOBS_COMMAND,
     EXCEPTION_COMMAND,
     UPDATE_COMMANDS,
     GET_OUT,
 ])
 MY_GAMES_COMMAND = BotCommand(command="my_games", description="мои игры (включая черновики)")
-NEW_LEVEL_COMMAND = BotCommand(command="new_level", description="новый уровень")
+NEW_LEVEL_COMMAND = BotCommand(command="new_level", description="новый уровень")  # TODO
 NEW_GAME_COMMAND = BotCommand(command="new_game", description="начать сборку новой игры из ранее написанных уровней")
+LEVELS_COMMAND = BotCommand(command="levels", description="показать список уровней")  # TODO
 HELP_ORG = CommandsGroup("Команды для организаторов:", [
     MY_GAMES_COMMAND,
     NEW_GAME_COMMAND,
-    BotCommand(command="levels", description="показать список уровней"),
+    LEVELS_COMMAND,
     NEW_LEVEL_COMMAND,
     GET_WAIVERS_COMMAND,
     SPY_COMMAND,

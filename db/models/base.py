29bacca1170f86eea67fde6052270aa50ebfe07a
Yuriy Chebyshev <bomzheg@gmail.com>
2022-11-20 18:54:11 +0300

    add convention

--- db/models/base.py
+++ db/models/base.py
@@ -1,3 +1,11 @@
+from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+convention = {
+    'ix': 'ix__%(column_0_label)s',
+    'uq': 'uq__%(table_name)s__%(column_0_name)s',
+    'ck': 'ck__%(table_name)s__%(constraint_name)s',
+    'fk': 'fk__%(table_name)s__%(column_0_name)s__%(referred_table_name)s',
+    'pk': 'pk__%(table_name)s'
+}
+meta = MetaData(convention)
+Base = declarative_base(metadata=meta)

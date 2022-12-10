28c719e896f54b7e8dd213631e9a4408eacdb9a3
Yuriy Chebyshev <bomzheg@gmail.com>
2022-12-10 22:39:01 +0300

    add interface

---
+++ shvatka/dal/achievement.py
@@ -0,0 +1,12 @@
+from abc import ABCMeta
+
+from shvatka.dal.base import Committer
+from shvatka.models import enums, dto
+
+
+class AchievementAdder(Committer, metaclass=ABCMeta):
+    async def exist_type(self, achievement: enums.Achievement) -> bool:
+        raise NotImplementedError
+
+    async def add_achievement(self, achievement: dto.Achievement) -> None:
+        raise NotImplementedError
--- shvatka/services/achievement.py
+++ shvatka/services/achievement.py
@@ -1,8 +1,8 @@
-from db.dao.rdb.achievement import AchievementDAO
+from shvatka.dal.achievement import AchievementAdder
 from shvatka.models import dto, enums
 
 
-async def add_achievement(player: dto.Player, name: enums.Achievement, dao: AchievementDAO):
+async def add_achievement(player: dto.Player, name: enums.Achievement, dao: AchievementAdder):
     first = not await dao.exist_type(name)
     achievement_result = dto.Achievement(player=player, name=name, first=first)
     await dao.add_achievement(achievement_result)

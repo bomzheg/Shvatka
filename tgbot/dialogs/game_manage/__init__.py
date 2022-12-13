6df65fb160b39c1c4f33d09096c859f2726d6508
Yuriy Chebyshev <bomzheg@gmail.com>
2022-12-14 00:10:23 +0300

    fix (register dialog)

--- tgbot/dialogs/game_manage/__init__.py
+++ tgbot/dialogs/game_manage/__init__.py
@@ -1,8 +1,9 @@
 from aiogram_dialog import DialogRegistry
 
-from .dialogs import my_games, schedule_game_dialog
+from .dialogs import my_games, schedule_game_dialog, games
 
 
 def setup(registry: DialogRegistry):
+    registry.register(games)
     registry.register(my_games)
     registry.register(schedule_game_dialog)

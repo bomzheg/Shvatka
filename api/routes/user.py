277aab9bd7a1c81c24e6367549f2d98c9c5ae1be
Yuriy Chebyshev <bomzheg@gmail.com>
2023-01-15 11:19:25 +0300

    black reformat

--- api/routes/user.py
+++ api/routes/user.py
@@ -16,8 +16,7 @@ async def read_users_me(current_user: dto.User = Depends(get_current_user)) -> d
 
 
 async def read_user(
-    id_: int = Path(alias="id"),  # type: ignore[assignment]
-    dao: HolderDao = Depends(dao_provider)
+    id_: int = Path(alias="id"), dao: HolderDao = Depends(dao_provider)  # type: ignore[assignment]
 ) -> dto.User:
     return await get_user(id_, dao.user)
 

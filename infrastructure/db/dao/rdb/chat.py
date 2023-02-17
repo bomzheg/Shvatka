97bb6bfc0b6fd13af6ace6d2e02c9b610d529ac9
Yuriy Chebyshev <bomzheg@gmail.com>
2023-02-17 23:41:49 +0300

    refactoring upsert chat

--- infrastructure/db/dao/rdb/chat.py
+++ infrastructure/db/dao/rdb/chat.py
@@ -1,5 +1,5 @@
 from sqlalchemy import select
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from infrastructure.db.models import Chat
@@ -16,32 +16,18 @@ class ChatDao(BaseDAO[Chat]):
         return result.scalar_one()
 
     async def upsert_chat(self, chat: dto.Chat) -> dto.Chat:
-        try:
-            saved_chat = await self.get_by_tg_id(chat.tg_id)
-        except NoResultFound:
-            saved_chat = Chat(tg_id=chat.tg_id)
-        was_changed = update_fields(chat, saved_chat)
-        if was_changed:
-            self._save(saved_chat)
-            await self._flush(saved_chat)
-        return saved_chat.to_dto()
+        kwargs = dict(tg_id=chat.tg_id, title=chat.title, username=chat.username, type=chat.type)
+        saved_chat = await self.session.execute(
+            insert(Chat)
+            .values(**kwargs)
+            .on_conflict_do_update(
+                index_elements=(Chat.tg_id,), set_=kwargs, where=Chat.tg_id == chat.tg_id
+            )
+            .returning(Chat)
+        )
+        return saved_chat.scalar_one().to_dto()
 
     async def update_chat_id(self, chat: dto.Chat, new_id: int):
         chat_db = await self.get_by_tg_id(chat.tg_id)
         chat_db.tg_id = new_id
         self._save(chat_db)
-
-
-def update_fields(source: dto.Chat, target: Chat):
-    if all(
-        [
-            target.title == source.title,
-            target.username == source.username,
-            target.type == source.type,
-        ]
-    ):
-        return False
-    target.title = source.name
-    target.username = source.username
-    target.type = source.type
-    return True

ab23388ac46666a7dc2e50a67ad302ca37c92663
Yuriy Chebyshev <bomzheg@gmail.com>
2023-02-18 23:18:55 +0300

    add saver teams

--- infrastructure/crawler/teams/parse_teams.py
+++ infrastructure/crawler/teams/parse_teams.py
@@ -1,9 +1,12 @@
 import asyncio
+import json
 import typing
 from datetime import date, datetime
+from pathlib import Path
 from urllib.parse import urlparse, parse_qs
 
 from aiohttp import ClientSession
+from dataclass_factory import Factory, Schema, NameStyle
 from lxml import etree
 
 from infrastructure.crawler.auth import get_auth_cookie
@@ -11,12 +14,11 @@ from infrastructure.crawler.constants import TEAMS_URL
 from infrastructure.crawler.models.team import Player, Team
 
 
-async def get_all_teams():
+async def get_all_teams() -> list[Team]:
     async with ClientSession(cookies=await get_auth_cookie()) as session:
         teams_html_text = await download_teams(session)
         teams = await TeamsParser(teams_html_text, session=session).build()
-    for team in teams:
-        print(team)
+    return teams
 
 
 async def download_teams(session: ClientSession) -> str:
@@ -35,8 +37,7 @@ class TeamsParser:
         for team_element in self.html.xpath('//table//td[@class="row2"]/b/a'):
             team_name = team_element.text
             url = team_element.get("href")
-            games_element = team_element.xpath("../../../td[4]")[0]
-            games = list(map(int, etree.tostring(games_element, method="text").decode("utf8").split()))
+            games = get_games_numbers(team_element.xpath("../../../td[4]")[0])
             team_id = int(parse_qs(typing.cast(str, urlparse(url).query))["id"][0])
             team = Team(
                 name=team_name,
@@ -48,7 +49,7 @@ class TeamsParser:
             self.teams.append(team)
 
     async def parse_team_players(self, team_url: str) -> list[Player]:
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(0.5)
         players = []
         async with self.session.get(team_url) as resp:
             resp.raise_for_status()
@@ -57,8 +58,8 @@ class TeamsParser:
             url = player_element.get("href")
             name = player_element.text
             root = player_element.xpath("../../..")[0]
-            role = root.xpath("td[2]")
-            games = list(map(int, etree.tostring(root.xpath("td[3]")[0], method="text").decode("utf8").split()))
+            role = root.xpath("td[2]")[0].text
+            games = get_games_numbers(root.xpath("td[4]")[0])
             player = Player(
                 name=name,
                 role=role,
@@ -70,7 +71,7 @@ class TeamsParser:
         return players
 
     async def parse_player_registered_date(self, url: str) -> date:
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(0.5)
         async with self.session.get(url) as resp:
             resp.raise_for_status()
             player = etree.HTML(await resp.text(encoding="cp1251", errors="backslashreplace"), base_url="shvatka.ru")
@@ -82,5 +83,17 @@ class TeamsParser:
         return self.teams
 
 
+def get_games_numbers(games_element) -> list[int]:
+    return list(map(int, etree.tostring(games_element, method="text").decode("utf8").split()))
+
+
+async def main_team_parser():
+    teams = await get_all_teams()
+    dcf = Factory(default_schema=Schema(name_style=NameStyle.kebab))
+    path = Path(__file__).parent
+    with open(path / "teams.json", "w", encoding="utf8") as f:
+        json.dump(dcf.dump(teams), f, ensure_ascii=False, indent=2)
+
+
 if __name__ == '__main__':
-    asyncio.run(get_all_teams())
+    asyncio.run(main_team_parser())

f5e4345117ea1be7a80483cb2334d5e7f6bba81d
Yuriy Chebyshev <bomzheg@gmail.com>
2023-03-11 21:43:25 +0300

    fix emoji choice by role

--- shvatka/tgbot/views/player.py
+++ shvatka/tgbot/views/player.py
@@ -6,5 +6,5 @@ def get_emoji(pit: dto.TeamPlayer) -> str:
     if pit.emoji:
         return pit.emoji
     if pit.role:
-        return EMOJI_BY_ROLE.get(pit.role, DEFAULT_EMOJI)
+        return EMOJI_BY_ROLE.get(pit.role.lower(), DEFAULT_EMOJI)
     return DEFAULT_EMOJI

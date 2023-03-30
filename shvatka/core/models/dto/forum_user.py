ab09682b5b3983d0c6a6bfa7af8f9c00be7187fc
Yuriy Chebyshev <bomzheg@gmail.com>
2023-03-31 00:28:20 +0300

    fixed forum user name mention

--- shvatka/core/models/dto/forum_user.py
+++ shvatka/core/models/dto/forum_user.py
@@ -11,4 +11,4 @@ class ForumUser:
 
     @property
     def name_mention(self) -> str:
-        return self.name or f"forum-user-{self.forum_id}" if self.forum_id else str(self.db_id)
+        return self.name or (f"forum-user-{self.forum_id}" if self.forum_id else str(self.db_id))

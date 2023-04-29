f52c9db1f9e0604e42f6f98e317ca8c19f2f710a
Yuriy Chebyshev <bomzheg@gmail.com>
2023-04-29 15:56:32 +0300

    added forgotten commit

--- shvatka/infrastructure/file_id_updater.py
+++ shvatka/infrastructure/file_id_updater.py
@@ -6,3 +6,4 @@ async def fill_all_file_id(dao: FileInfoDao, file_gateway: BotFileGateway):
     while batch := await dao.get_without_file_id(100):
         for file in batch:
             await file_gateway.renew_file_id(file.author, file)
+        await dao.commit()

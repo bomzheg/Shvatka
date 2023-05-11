5716f141f28dcc57baf7b3c1957da032c5eeed27
Yuriy Chebyshev <bomzheg@gmail.com>
2023-05-11 19:09:47 +0300

    fix is key filter

--- shvatka/tgbot/filters/is_key.py
+++ shvatka/tgbot/filters/is_key.py
@@ -4,6 +4,7 @@ from shvatka.core.utils.input_validation import normalize_key
 
 
 def is_key(message: Message):
-    assert isinstance(message.text, str)
+    if not message.text:
+        return False
     key = normalize_key(message.text)
     return {"key": key} if key is not None else False

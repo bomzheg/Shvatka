0a76d39263cab3eea0cdbd420ca952cf23ac6869
ychebyshev <ychebyshev@solit-clouds.ru>
2024-01-07 23:06:19 +0300

    fixed linters

--- shvatka/tgbot/config/models/bot.py
+++ shvatka/tgbot/config/models/bot.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from enum import Enum
 
 from aiogram.client.session.aiohttp import AiohttpSession

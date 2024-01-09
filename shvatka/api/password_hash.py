1bf44234ade6e2ed0d590fff8fe92a249c752f1d
ychebyshev <ychebyshev@solit-clouds.ru>
2024-01-09 22:53:05 +0300

    added generate password util

---
+++ shvatka/api/password_hash.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+
+from shvatka.api.config.parser.main import load_config
+from shvatka.api.dependencies import AuthProvider
+from shvatka.api.main_factory import (
+    get_paths,
+)
+from shvatka.common.config.parser.logging_config import setup_logging
+
+logger = logging.getLogger(__name__)
+
+
+def generate():
+    paths = get_paths()
+
+    setup_logging(paths)
+    config = load_config(paths)
+    auth = AuthProvider(config.auth)
+    return auth.get_password_hash(sys.argv[1])
+
+
+if __name__ == "__main__":
+    print(generate())  # noqa: T201

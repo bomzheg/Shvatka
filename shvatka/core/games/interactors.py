c3b8e2f1c60933c81f8d2561e24ab52a4ffc2d32
ychebyshev <ychebyshev@solit-clouds.ru>
2024-03-14 09:03:53 +0300

    added check for full game are accessible

--- shvatka/core/games/interactors.py
+++ shvatka/core/games/interactors.py
@@ -3,6 +3,7 @@ from typing import BinaryIO
 from shvatka.core.interfaces.clients.file_storage import FileGateway
 from shvatka.core.interfaces.dal.complex import GameFileLoader
 from shvatka.core.models import dto
+from shvatka.core.rules.game import check_can_read
 from shvatka.core.services.scenario.files import check_file_meta_can_read
 from shvatka.core.utils import exceptions
 
@@ -13,8 +14,9 @@ class FileReader:
         self.dao = dao
 
     async def __call__(self, guid: str, game_id: int, user: dto.User) -> BinaryIO:
-        author = await self.dao.get_by_user(user)
+        player = await self.dao.get_by_user(user)
         game = await self.dao.get_full(game_id)
+        check_can_read(game, player)
         if guid not in game.get_guids():
             raise exceptions.FileNotFound(
                 text=f"There is no file with uuid {guid} associated with game id {game_id}",
@@ -22,9 +24,9 @@ class FileReader:
                 game=game,
                 user_id=user.db_id,
                 user=user,
-                player_id=author.id,
-                player=author,
+                player_id=player.id,
+                player=player,
             )
         meta = await self.dao.get_by_guid(guid)
-        check_file_meta_can_read(author, meta, game)
+        check_file_meta_can_read(player, meta, game)
         return await self.file_gateway.get(meta)

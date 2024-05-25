9c9a8666e08520911b8ae97e0d0bd8f3a8a455f8
Yuriy Chebyshev <bomzheg@gmail.com>
2024-05-25 13:00:50 +0300

    update aiogram to 3.4.1

--- shvatka/infrastructure/di/bot.py
+++ shvatka/infrastructure/di/bot.py
@@ -1,4 +1,5 @@
 from aiogram import Bot
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dishka import Provider, Scope, provide
 
@@ -13,8 +14,11 @@ class BotProvider(Provider):
     def get_bot(self, config: BotConfig) -> Bot:
         bot = Bot(
             token=config.token,
-            parse_mode=ParseMode.HTML,
             session=config.create_session(),
+            default=DefaultBotProperties(
+                parse_mode=ParseMode.HTML,
+                allow_sending_without_reply=True,
+            ),
         )
         setup_jinja(bot)
         return bot

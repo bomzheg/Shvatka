431bcad0626ec03e6f9a183681f2000c67dc3a44
Yuriy Chebyshev <bomzheg@gmail.com>
2024-06-11 20:00:50 +0300

    hotfix none game for main menu

--- shvatka/tgbot/dialogs/main_menu/getters.py
+++ shvatka/tgbot/dialogs/main_menu/getters.py
@@ -17,7 +17,10 @@ async def get_main(dao: HolderDao, player: dto.Player, game: dto.Game, **_):
     try:
         team = await get_my_team(player=player, dao=dao.team_player)
         team_player = await get_full_team_player(player=player, team=team, dao=dao.team_player)
-        waiver = await get_my_waiver(player=player, team=team, game=game, dao=dao.waiver)
+        if game:
+            waiver = await get_my_waiver(player=player, team=team, game=game, dao=dao.waiver)
+        else:
+            waiver = None
     except PlayerNotInTeam:
         team = None
         team_player = None

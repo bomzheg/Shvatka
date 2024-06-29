dbf9c25fb63f556c9f3d7be2a3f558e2423b0faf
Yuriy Chebyshev <bomzheg@gmail.com>
2024-06-29 10:03:34 +0300

    fixed timezone filter for None

--- shvatka/tgbot/views/jinja_filters/timezone.py
+++ shvatka/tgbot/views/jinja_filters/timezone.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from shvatka.core.utils.datetime_utils import tz_game, DATETIME_FORMAT
 
 
-def datetime_filter(value: datetime, format_: str = DATETIME_FORMAT) -> str:
+def datetime_filter(value: datetime | None, format_: str = DATETIME_FORMAT) -> str:
+    if value is None:
+        return "n/a"
     local_dt = value.astimezone(tz_game)
     return local_dt.strftime(format_)

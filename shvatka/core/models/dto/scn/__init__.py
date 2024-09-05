737a4966d3eb540eda7cca8c862ceacfb86b9a60
bomzheg <bomzheg@gmail.com>
2024-09-05 09:41:11 +0300

    added HintsList

--- shvatka/core/models/dto/scn/__init__.py
+++ shvatka/core/models/dto/scn/__init__.py
@@ -25,6 +25,6 @@ from .hint_part import (
     PhotoHint,
     ContactHint,
 )
-from .level import LevelScenario, SHKey, BonusKey
+from .level import LevelScenario, SHKey, BonusKey, HintsList
 from .parsed_zip import ParsedZip
 from .time_hint import TimeHint

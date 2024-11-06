7d13e08221242c60209dcf36eb0412c6d2389b2b
bomzheg <bomzheg@gmail.com>
2024-11-06 11:46:10 +0300

    #106 added few more tests

--- tests/unit/domain/conditions_test.py
+++ tests/unit/domain/conditions_test.py
@@ -25,3 +25,10 @@ def test_create_empty_condition():
 def test_create_only_bonus_condition():
     with pytest.raises(exceptions.LevelError):
         scn.Conditions([action.KeyBonusCondition({keys.BonusKey(text="SH123", bonus_minutes=1)})])
+
+def test_conditions_get_keys():
+    c = scn.Conditions([
+        action.KeyWinCondition({keys.SHKey("SH123"), keys.SHKey("SH321")}),
+        action.KeyWinCondition({keys.SHKey("СХ123")})
+    ])
+    assert c.get_keys() == {keys.SHKey("SH123"), keys.SHKey("SH321"), keys.SHKey("СХ123")}

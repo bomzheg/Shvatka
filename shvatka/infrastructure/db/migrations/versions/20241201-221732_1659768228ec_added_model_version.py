8537d0a256c3862cc84952b2e75be1776e1c1c71
bomzheg <bomzheg@gmail.com>
2024-12-01 22:22:02 +0300

    added model-version to legacy levels

---
+++ shvatka/infrastructure/db/migrations/versions/20241201-221732_1659768228ec_added_model_version.py
@@ -0,0 +1,50 @@
+"""added model-version
+
+Revision ID: 1659768228ec
+Revises: 74618499d318
+Create Date: 2024-12-01 22:17:32.958776
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '1659768228ec'
+down_revision = '74618499d318'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+       WITH scn AS (
+        SELECT jsonb_insert(
+           l.scenario::JSONB,
+           '{__model_version__}',
+           '1'
+        ) AS scenario,
+        l.id
+        FROM levels AS l
+    )
+    UPDATE levels lvl
+    SET scenario = scn.scenario
+    FROM scn
+    WHERE scn.id = lvl.id 
+    """)
+
+
+def downgrade():
+    op.execute("""
+       WITH scn AS (
+        SELECT 
+           l.scenario::JSONB - '__model_version__' AS scenario,
+        l.id
+        FROM levels AS l
+    )
+    UPDATE levels lvl
+    SET scenario = scn.scenario
+    FROM scn
+    WHERE scn.id = lvl.id 
+    """)
+

f8c6a9a370080417bf72b6b242aec2d9148c10d7
Yuriy Chebyshev <bomzheg@gmail.com>
2025-03-29 21:41:49 +0300

    replace into dishka

--- shvatka/tgbot/dialogs/time_hint/handlers.py
+++ shvatka/tgbot/dialogs/time_hint/handlers.py
@@ -5,8 +5,8 @@ from aiogram import types
 from aiogram.types import CallbackQuery, Message
 from aiogram_dialog import DialogManager, SubManager
 from aiogram_dialog.widgets.kbd import Button
-from dishka import AsyncContainer
-from dishka.integrations.aiogram import CONTAINER_NAME
+from dishka import FromDishka
+from dishka.integrations.aiogram_dialog import inject
 
 from shvatka.core.models.dto import hints
 from shvatka.core.utils import exceptions
@@ -19,13 +19,18 @@ async def select_time(c: CallbackQuery, widget: Any, manager: DialogManager, ite
     await set_time(int(item_id), manager)
 
 
-async def process_edit_time_message(m: Message, dialog_: Any, manager: DialogManager) -> None:
+@inject
+async def process_edit_time_message(
+    m: Message,
+    dialog_: Any,
+    manager: DialogManager,
+    retort: FromDishka[Retort]
+) -> None:
     try:
         time_ = int(m.text)
     except ValueError:
         await m.answer("Некорректный формат времени. Пожалуйста введите время в формате ЧЧ:ММ")
         return
-    retort: Retort = manager.middleware_data["retort"]
     hint = retort.load(manager.start_data["time_hint"], hints.TimeHint)
     if not hint.can_update_time():
         await m.reply(
@@ -47,21 +52,29 @@ async def process_time_message(m: Message, dialog_: Any, manager: DialogManager)
     await set_time(time_, manager)
 
 
-async def edit_single_hint(c: CallbackQuery, widget: Any, manager: DialogManager):
+@inject
+async def edit_single_hint(
+    c: CallbackQuery,
+    widget: Any,
+    manager: DialogManager,
+    retort: FromDishka[Retort],
+    hint_sender: FromDishka[HintSender],
+):
     assert isinstance(manager, SubManager)
-    dishka: AsyncContainer = manager.middleware_data[CONTAINER_NAME]
-    retort = await dishka.get(Retort)
     hint = retort.load(manager.dialog_data["hints"], list[hints.AnyHint])
-    hint_sender = await dishka.get(HintSender)
     chat: types.Chat = manager.middleware_data["event_chat"]
     hint_index = manager.item_id
     await hint_sender.send_hint(hint[int(hint_index)], chat.id)
 
 
-async def delete_single_hint(c: CallbackQuery, widget: Any, manager: DialogManager):
+@inject
+async def delete_single_hint(
+    c: CallbackQuery,
+    widget: Any,
+    manager: DialogManager,
+    retort: FromDishka[Retort],
+):
     assert isinstance(manager, SubManager)
-    dishka: AsyncContainer = manager.middleware_data[CONTAINER_NAME]
-    retort = await dishka.get(Retort)
     hints_ = retort.load(manager.dialog_data.get("hints"), list[hints.AnyHint])
     hint_index = manager.item_id
     hints_.pop(int(hint_index))
@@ -72,9 +85,13 @@ async def delete_whole_time_hint(c: CallbackQuery, widget: Any, manager: DialogM
     await manager.done({"edited_time_hint": {"__deleted__": "__deleted_true__"}})
 
 
-async def save_edited_time_hint(c: CallbackQuery, widget: Any, manager: DialogManager):
-    dishka: AsyncContainer = manager.middleware_data[CONTAINER_NAME]
-    retort = await dishka.get(Retort)
+@inject
+async def save_edited_time_hint(
+    c: CallbackQuery,
+    widget: Any,
+    manager: DialogManager,
+    retort: FromDishka[Retort]
+):
     time_hint = retort.load(manager.start_data["time_hint"], hints.TimeHint)
     try:
         time_hint.update_time(manager.dialog_data["time"])
@@ -94,15 +111,25 @@ async def set_time(time_minutes: int, manager: DialogManager):
     await manager.switch_to(states.TimeHintSG.hint)
 
 
-async def process_hint(m: Message, dialog_: Any, manager: DialogManager) -> None:
-    retort: Retort = manager.middleware_data["retort"]
-    parser: HintParser = manager.middleware_data["hint_parser"]
+@inject
+async def process_hint(
+    m: Message,
+    dialog_: Any,
+    manager: DialogManager,
+    retort: FromDishka[Retort],
+    parser: FromDishka[HintParser],
+) -> None:
     hint = await parser.parse(m, manager.middleware_data["player"])
     manager.dialog_data["hints"].append(retort.dump(hint))
 
 
-async def on_finish(c: CallbackQuery, button: Button, manager: DialogManager):
-    retort: Retort = manager.middleware_data["retort"]
+@inject
+async def on_finish(
+    c: CallbackQuery,
+    button: Button,
+    manager: DialogManager,
+    retort: FromDishka[Retort],
+):
     hints_ = retort.load(manager.dialog_data["hints"], list[hints.AnyHint])
     time_ = manager.dialog_data["time"]
     time_hint = hints.TimeHint(time=time_, hint=hints_)
@@ -117,8 +144,12 @@ async def hint_on_start(start_data: dict, manager: DialogManager):
     manager.dialog_data.setdefault("hints", [])
 
 
-async def hint_edit_on_start(start_data: dict, manager: DialogManager):
-    retort: Retort = manager.middleware_data["retort"]
+@inject
+async def hint_edit_on_start(
+    start_data: dict,
+    manager: DialogManager,
+    retort: FromDishka[Retort],
+):
     hint = retort.load(manager.start_data["time_hint"], hints.TimeHint)
     manager.dialog_data["hints"] = retort.dump(hint.hint, list[hints.AnyHint])
     manager.dialog_data["time"] = hint.time

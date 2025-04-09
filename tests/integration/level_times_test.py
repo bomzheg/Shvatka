5c2e9f1f006bd0527878552fc847b7441f85c85d
Yuriy Chebyshev <bomzheg@gmail.com>
2025-04-09 22:41:50 +0300

    added test for results

--- tests/integration/level_times_test.py
+++ tests/integration/level_times_test.py
@@ -4,7 +4,8 @@ import pytest
 
 from shvatka.core.models import dto
 from shvatka.core.utils.datetime_utils import trim_tz
-from shvatka.tgbot.views.results.level_times import results_to_table_linear, to_results, GAME_NAME, FIRST_TEAM_NAME
+from shvatka.tgbot.views.results.level_times import results_to_table_linear, to_results, GAME_NAME, FIRST_TEAM_NAME, \
+    print_table, Table
 
 
 def test_to_results(finished_game: dto.FullGame, game_stat: dto.GameStat, gryffindor: dto.Team, slytherin: dto.Team):
@@ -15,14 +16,14 @@ def test_to_results(finished_game: dto.FullGame, game_stat: dto.GameStat, gryffi
     assert table[FIRST_TEAM_NAME].value == gryffindor.name
     base_time = trim_tz(finished_game.start_at)
     assert table[FIRST_TEAM_NAME.shift(plus_columns=1)].value == base_time + timedelta(minutes=20)
-    #assert table[FIRST_TEAM_NAME.shift(plus_columns=2)].value == base_time + timedelta(minutes=30)
+    assert table[FIRST_TEAM_NAME.shift(plus_columns=2)].value == base_time + timedelta(minutes=30)
     assert table[FIRST_TEAM_NAME.shift(plus_rows=1)].value == slytherin.name
     assert table[FIRST_TEAM_NAME.shift(plus_rows=1, plus_columns=1)].value == base_time + timedelta(minutes=10)
     assert table[FIRST_TEAM_NAME.shift(plus_rows=1, plus_columns=2)].value == base_time + timedelta(minutes=40)
     assert table[FIRST_TEAM_NAME.shift(plus_rows=3, plus_columns=1)].value == 1
-    #assert table[FIRST_TEAM_NAME.shift(plus_rows=3, plus_columns=2)].value == 2
+    assert table[FIRST_TEAM_NAME.shift(plus_rows=3, plus_columns=2)].value == 2
     assert table[FIRST_TEAM_NAME.shift(plus_rows=4)].value == gryffindor.name
-    #assert table[FIRST_TEAM_NAME.shift(plus_rows=4, plus_columns=1)].value == time(minute=20)
+    assert table[FIRST_TEAM_NAME.shift(plus_rows=4, plus_columns=1)].value == time(minute=20)
     assert table[FIRST_TEAM_NAME.shift(plus_rows=4, plus_columns=2)].value == time(minute=30)
     assert table[FIRST_TEAM_NAME.shift(plus_rows=5)].value == slytherin.name
     assert table[FIRST_TEAM_NAME.shift(plus_rows=5, plus_columns=1)].value == time(minute=10)

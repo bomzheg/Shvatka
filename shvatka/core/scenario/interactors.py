1afa4f6870bfbf9858a881a54e3b209223d895a7
Yuriy Chebyshev <bomzheg@gmail.com>
2025-05-01 14:30:29 +0300

    added presenter and usecase

---
+++ shvatka/core/scenario/interactors.py
@@ -0,0 +1,75 @@
+from typing import BinaryIO
+
+from shvatka.core.interfaces.dal.game import GameByIdGetter
+from shvatka.core.models import dto as core
+from shvatka.core.models.dto import action
+from shvatka.core.scenario import dto
+from shvatka.tgbot.views.utils import render_hints
+
+
+class AllGameKeysReaderInteractor:
+    def __init__(self, dao: GameByIdGetter):
+        self.dao = dao
+
+    async def __call__(self, game_id: int) -> BinaryIO:
+        game = await self.dao.get_full(game_id)
+        return self.view(game, self.presenter(game))
+
+    def view(self, game: core.FullGame, keys: list[dto.LevelKeys]) -> BinaryIO:
+        pass
+
+    def presenter(self, game: core.FullGame) -> list[dto.LevelKeys]:
+        result = []
+        for level in game.levels:
+            assert level.number_in_game is not None
+            keys = []
+            for win_condition in level.scenario.conditions.get_default_key_conditions():
+                keys.extend(self.presenter_win_condition(win_condition, game.levels))
+            for routed_condition in level.scenario.conditions.get_routed_conditions():
+                keys.extend(self.presenter_win_condition(routed_condition, game.levels))
+            for bonus_hint_condition in level.scenario.conditions.get_bonus_hints_conditions():
+                keys.extend(self.presenter_bonus_hint_condition(bonus_hint_condition))
+            for bonus_condition in level.scenario.conditions.get_bonus_conditions():
+                keys.extend(self.presenter_bonus_condition(bonus_condition))
+            lk = dto.LevelKeys(
+                level_number=level.number_in_game,
+                level_name_id=level.name_id,
+                keys=keys,
+            )
+            result.append(lk)
+        return result
+
+    def presenter_win_condition(
+        self, condition: action.KeyWinCondition, levels: list[core.Level]
+    ) -> list[dto.Key]:
+        if condition.next_level:
+            next_level = next(level for level in levels if level.name_id == condition.next_level)
+            return [
+                dto.Key(
+                    keys=condition.keys,
+                    description=f"-> {next_level.name_id} ({next_level.number_in_game})",
+                )
+            ]
+        else:
+            return [
+                dto.Key(
+                    keys=condition.keys,
+                    description="",
+                )
+            ]
+
+    def presenter_bonus_hint_condition(
+        self, condition: action.KeyBonusHintCondition
+    ) -> list[dto.Key]:
+        return [
+            dto.Key(
+                keys=condition.keys,
+                description=f"bonus hint: {render_hints(condition.bonus_hint)}",
+            )
+        ]
+
+    def presenter_bonus_condition(self, condition: action.KeyBonusCondition) -> list[dto.Key]:
+        return [
+            dto.Key(keys={bk.text}, description=f"{bk.bonus_minutes} мин.")
+            for bk in condition.keys
+        ]

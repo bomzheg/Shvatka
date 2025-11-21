cd47a342b1728472ff3003d2898cb5b9de17833f
Yuriy Chebyshev <bomzheg@gmail.com>
2025-11-21 19:32:03 +0300

    try to create a loop first

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,3 +1,11 @@
+import asyncio
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
 import logging
 import typing
 from unittest.mock import MagicMock

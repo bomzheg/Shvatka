5151c5270d30cef5e7e1eb1180200ec3ef67f51c
Yuriy Chebyshev <bomzheg@gmail.com>
2026-04-24 23:03:12 +0300

    try to fix token expiration

--- shvatka/api/config/parser/auth.py
+++ shvatka/api/config/parser/auth.py
@@ -6,7 +6,7 @@ from shvatka.api.config.models.auth import AuthConfig
 def load_auth(dct: dict) -> AuthConfig:
     return AuthConfig(
         secret_key=dct["secret-key"],
-        token_expire=timedelta(minutes=dct["token-expire-minutes"]),
+        token_expire=timedelta(minutes=int(dct["token-expire-minutes"])),
         bot_token=dct["bot-token"],
         bot_username=dct["bot-username"],
         auth_url=dct["auth-url"],

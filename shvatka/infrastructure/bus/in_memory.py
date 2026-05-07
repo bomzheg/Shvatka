db945fbee3317dc0a4fc3a9f9afba80289ad98df
Yuriy Chebyshev <bomzheg@gmail.com>
2026-05-07 23:59:54 +0300

    do not throw after bus

--- shvatka/infrastructure/bus/in_memory.py
+++ shvatka/infrastructure/bus/in_memory.py
@@ -1,8 +1,11 @@
+import logging
 from abc import abstractmethod, ABCMeta
 from dataclasses import dataclass
 from typing import Protocol
 
 from shvatka.core.interfaces.bus import Bus, Event, OneTimeTokenUsed
+
+logger = logging.getLogger(__name__)
 
 
 class UsedOneTimeTokenInteractor(Protocol, metaclass=ABCMeta):
@@ -16,6 +19,10 @@ class InMemoryBus(Bus):
     one_time_token: UsedOneTimeTokenInteractor
 
     async def submit(self, event: Event) -> None:
-        match event:
-            case OneTimeTokenUsed(player_id=player_id):
-                await self.one_time_token(player_id=player_id)
+        try:
+            match event:
+                case OneTimeTokenUsed(player_id=player_id):
+                    await self.one_time_token(player_id=player_id)
+        except Exception as e:
+            logger.error("error while processing event %s", event, exc_info=e)
+

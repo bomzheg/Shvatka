791940236b843e7c5f15dd1f70141a78cd2435fc
Yuriy Chebyshev <bomzheg@gmail.com>
2026-07-03 22:08:26 +0300

    fixed method

--- shvatka/core/interfaces/one_time_token.py
+++ shvatka/core/interfaces/one_time_token.py
@@ -2,5 +2,5 @@ import typing
 
 
 class OneTimeTokenCreator(typing.Protocol):
-    async def save_new_token(self, **dct: dict) -> str:
+    async def save_new_token(self, *, token_len: int = ..., expire: int = ..., **dct: dict) -> str:
         raise NotImplementedError
